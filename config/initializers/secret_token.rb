1ad60e99174604b48cf62dff081eeac34098472a
Joe Winter <jwinter@gmail.com>
2014-04-03 21:01:50 -0400

    Initial commit; rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ridicuclock::Application.config.secret_key_base = 'd3d26a3d5027f775a40a7504e1a7d1b3244d9c64117fcb7925e67072c46a4e03733c4e71828dc3f4a1951907385b37c19d031aac5e0816aa19feac8aa38e25d8'
