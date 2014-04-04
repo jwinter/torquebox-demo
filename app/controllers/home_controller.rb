21bbb0a1a24429bc9ce1b3ab4b6aa27bbc9ad93e
jwinter <jwinter@gmail.com>
2014-04-04 11:43:30 -0400

    Include other Infinispan

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
     @time = Rails.cache.fetch('times-that-try-mens-souls', expires_in: 2.minutes) { Time.now }
-    @endpoint = TorqueBox.fetch('stomp-endpoint')
+    other_cache = TorqueBox::Infinispan::Cache.new(name: 'ridicuclock', mode: :distributed)
+    other_cache.put('keyname', {foo: 'bar'}.to_json)
+    @other_cache_value = other_cache.get('keyname')
+
+    @endpoint = TorqueBox.fetch('stomp-endpoint') # This is using their Dependency Injection framework
   end
 end
